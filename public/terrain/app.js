949501f42ee1b697c06cf58b8bbb1d552629f352
jumpt57 <jumpt57@github.com>
2016-11-02 09:40:42 +0100

    Maj

--- public/terrain/app.js
+++ public/terrain/app.js
@@ -1,4 +1,4 @@
- //------------------------------------------------------
+    //------------------------------------------------------
     // Globales variables
     //------------------------------------------------------
     var MAX_HEIGHT = 10;
@@ -7,8 +7,8 @@
     var camera;
     var control;
     var stats;
-    var scale = chroma.scale(['blue','green','red']).domain([0,MAX_HEIGHT]);
-    
+    var ground;
+
     //------------------------------------------------------
     // Main
     //------------------------------------------------------
@@ -18,24 +18,39 @@
      */
     function init() {
         // create a scene, that will hold all our elements such as objects, cameras and lights.
-        scene = new THREE.Scene();
+        //scene = new THREE.Scene();        
+        scene = new Physijs.Scene;
+        scene.setGravity(new THREE.Vector3(0,-50,0));
+
         // create a camera, which defines where we're looking at.
         camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+        // position and point the camera to the center of the scene
+        camera.position.x = 100;
+        camera.position.y = 150;
+        camera.position.z = 450;
+        
+        camera.near = 500;
+        camera.far = 4000;
+        camera.fov = 30;
+        camera.lookAt(scene.position);      
+
         // create a render, sets the background color and the size
         renderer = new THREE.WebGLRenderer();
         renderer.setClearColor(0x000000, 1.0);
         renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.shadowMapEnabled = true;
-        // position and point the camera to the center of the scene
-        camera.position.x = 100;
-        camera.position.y = 100;
-        camera.position.z = 100;
-        camera.lookAt(scene.position);
+        renderer.shadowMapEnabled = true; 
+
+         // add controls
+        cameraControl = new THREE.OrbitControls(camera, renderer.domElement);      
+
         // add spotlight for the shadows
         var spotLight = new THREE.SpotLight(0xffffff);
-        spotLight.position.set(10, 300, 10);
+        spotLight.position.set(100, 150, 450);  
+        spotLight.castShadow = true;
+  
         scene.add(spotLight);
         scene.add(new THREE.AmbientLight(0x252525));
+
         // setup the control object for the control gui
         control = new function() {
             this.toFaceMaterial = function(){
@@ -63,13 +78,18 @@
                 geom.normalsNeedUpdate = true;
             }
         };
+
         // add the control gui and the stats UI
         addControlGui(control);
         addStatsObject();
+
         // add the output of the renderer to the html element
         document.body.appendChild(renderer.domElement);
+
         // set up the example specific stuff
         create3DTerrain(140, 140, 2.5, 2.5, MAX_HEIGHT);
+        createSphere(10);
+
         // call the render function, after the first render, interval is determined
         // by requestAnimationFrame
         render();
@@ -79,66 +99,57 @@
     //------------------------------------------------------
     function create3DTerrain(width, depth, spacingX, spacingZ, height) {
         var date = new Date();
-
+        var scale = chroma.scale(['blue','green','red']).domain([0,MAX_HEIGHT]);
         noise.seed(date.getMilliseconds());
-        // first create all the individual vertices
-        var geometry = new THREE.Geometry();
-        for (var z = 0 ; z < depth ; z++) {
-            for (var x = 0 ; x < width ; x++) {
-                var yValue = Math.abs(noise.perlin2(x/7, z/7) * height*2);
-                var vertex = new THREE.Vector3(x*spacingX, yValue,z*spacingZ);
-                geometry.vertices.push(vertex);
-            }
+      
+        var plane = new THREE.PlaneGeometry(1200,1000,100,100);
+
+        for (var i = 0; i < plane.vertices.length;i++) {
+            var vertex = plane.vertices[i];
+            var value = Math.abs(noise.perlin2(vertex.x / 10, vertex.y / 10, 0)* height*2);
+            vertex.z = value * 4;
         }
-        // next we need to define the faces. Which are triangles
-        // we create a rectangle between four vertices, and we do
-        // that as two triangles.
-        for (var z = 0 ; z < depth-1 ; z++) {
-            for (var x = 0 ; x < width-1 ; x++) {
-                // we need to point to the position in the array
-                // a - - b
-                // |  x  |
-                // c - - d
-                var a = x + z*width;
-                var b = (x+1) + (z * width);
-                var c = x + ((z+1) * width);
-                var d = (x+1) + ((z+1) * width);
-                // define the uvs for the vertices we just created.
-                var uva = new THREE.Vector2( x / (width-1) ,1 - z / (depth - 1) );
-                var uvb = new THREE.Vector2( (x + 1) / (width-1) ,1 - z / (depth - 1) );
-                var uvc = new THREE.Vector2( x / (width-1) ,1 - (z + 1) / (depth - 1) );
-                var uvd = new THREE.Vector2( (x + 1) / (width-1) ,1 - (z + 1) / (depth - 1) );
-                var face1 = new THREE.Face3(b, a, c );
-                var face2 = new THREE.Face3(c ,d, b );
-                face1.color = new THREE.Color(scale(getHighPoint(geometry, face1)).hex());
-                face2.color = new THREE.Color(scale(getHighPoint(geometry, face2)).hex())
-                geometry.faces.push(face1);
-                geometry.faces.push(face2);
-                geometry.faceVertexUvs[ 0 ].push( [ uvb, uva, uvc ] );
-                geometry.faceVertexUvs[ 0 ].push( [ uvc, uvd, uvb ] );
-            }
-        }
-        // compute the normals
-        geometry.computeVertexNormals(true);
-        geometry.computeFaceNormals();
-        // setup the material
+
+        plane.computeVertexNormals(true);
+        plane.computeFaceNormals();
+
+        plane.receiveShadow = true;
+
         var mat = new THREE.MeshPhongMaterial();
         mat.map = THREE.ImageUtils.loadTexture("./textures/ground/grasslight-big.jpg");
-    //    mat.map = THREE.ImageUtils.loadTexture("../assets/textures/debug.jpg");
-        // create the mesh
-        var groundMesh = new THREE.Mesh(geometry,mat);
-        groundMesh.translateX(-width/1.5);
-        groundMesh.translateZ(-depth/4);
-        groundMesh.translateY(50);
-        groundMesh.name = 'terrain';
-        scene.add(groundMesh);
+
+
+        var ground = new Physijs.HeightfieldMesh(
+            plane,
+            mat,
+            0
+        );
+        ground.name = 'terrain';
+        ground.rotation.x = -Math.PI/2;
+        ground.receiveShadow = true;
+        scene.add(ground);
     }
+
+    function createSphere(number){       
+        for(var i = 0; i < number; i++){
+            var box = new Physijs.SphereMesh(
+                new THREE.SphereGeometry( 10, 32, 32 ),
+                new THREE.MeshBasicMaterial({ color: 121212 })
+            );
+            box.position.x = box.position.x + Math.floor(Math.random() * 100) + 1;
+            box.position.y = box.position.y + 200;
+            box.position.z = box.position.z + Math.floor(Math.random() * 100) + 1;
+            scene.add( box );
+        }
+    }
+
     function getHighPoint(geometry, face) {
         var v1 = geometry.vertices[face.a].y;
         var v2 = geometry.vertices[face.b].y;
         var v3 = geometry.vertices[face.c].y;
         return Math.max(v1, v2, v3);
     }
+
     //------------------------------------------------------
     // Main render loop
     //------------------------------------------------------
@@ -148,17 +159,22 @@
      */
     function render() {
         // update the camera
-    //        var rotSpeed = control.rotationSpeed;
-    //        camera.position.x = camera.position.x * Math.cos(rotSpeed) + camera.position.z * Math.sin(rotSpeed);
-    //        camera.position.z = camera.position.z * Math.cos(rotSpeed) - camera.position.x * Math.sin(rotSpeed);
-    //        camera.lookAt(scene.position);
-        // update stats
-        stats.update();
+        //var rotSpeed = control.rotationSpeed;
+        //camera.position.x = camera.position.x * Math.cos(rotSpeed) + camera.position.z * Math.sin(rotSpeed);
+        //camera.position.z = camera.position.z * Math.cos(rotSpeed) - camera.position.x * Math.sin(rotSpeed);
+        //camera.lookAt(scene.position);       
+        
         // and render the scene
         renderer.render(scene, camera);
         // render using requestAnimationFrame
         requestAnimationFrame(render);
+
+         // update stats
+        stats.update();
+        scene.simulate();
+        cameraControl.update();
     }
+
     //------------------------------------------------------
     // Some generic helper functions
     //------------------------------------------------------
@@ -173,6 +189,7 @@
         gui.add(controlObject,'toNormalMaterial');
         gui.add(controlObject,'smoothShading').onChange(controlObject.onSmoothShadingChange);
     }
+
     /**
      * Add the stats object to the top left border
      */
@@ -184,6 +201,7 @@
         stats.domElement.style.top = '0px';
         document.body.appendChild( stats.domElement );
     }
+
     /**
      * Function handles the resize event. This make sure the camera and the renderer
      * are updated at the correct moment.
@@ -193,6 +211,7 @@
         camera.updateProjectionMatrix();
         renderer.setSize(window.innerWidth, window.innerHeight);
     }
+
     //------------------------------------------------------
     // Init the scene and register the resize handler
     //------------------------------------------------------
